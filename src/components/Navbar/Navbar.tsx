e63cbc0a2fd5096be82c25b4aa4b6d6da00ae449
pabloramoss <pablo-362@hotmail.com>
2023-11-28 22:40:18 +0100

    feat: add home linkg in navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,10 +1,16 @@
+import Link from 'next/link';
+
+import { Routes } from '@/lib/routes';
+
 const Navbar: React.FC = () => {
   return (
     <nav className="bg-white drop-shadow-lg py-2">
       <div className="flex items-center mx-auto justify-between max-w-5xl">
         <div>
           <img />
-          <p className="font-bold">Duogringo</p>
+          <Link className="font-bold" href={Routes.Home}>
+            Duogringo
+          </Link>
         </div>
         <div>
           <img className="rounded-full cursor-pointer" src="https://via.placeholder.com/40" />
